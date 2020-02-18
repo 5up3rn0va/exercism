384997e866e51552a4a15fdabbc8c85af0122c23
Sivakami Praveen <47029515+5up3rn0va@users.noreply.github.com>
2020-02-18 23:02:27 +0530

    Add files via upload

--- python/rna-transcription/rna_transcription.py
+++ python/rna-transcription/rna_transcription.py
@@ -1,8 +1,8 @@
-def to_rna(dna_strand):
-	nucleotides = {"G":"C", "C":"G", "T":"A", "A":"U"}
-	rna_strand = ""
-	
-	for nucleotide in dna_strand:
-		rna_strand += nucleotides[nucleotide]
+from string import maketrans
 
-	return rna_strand
+def to_rna(dna_strand):
+	intab = "GCTA"
+	outab = "CGAU"
+	trantab = maketrans(intab, outab)
+	
+	return dna_strand.translate(trantab)
